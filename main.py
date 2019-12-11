71d3fdd14b50d90e91156ba3eec54cc8138b8a07
Lucy <Air@MacBook-Air-Macbook.local>
2019-12-12 01:46:38 +0300

    -much better

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if mode == "dev":
         updater.start_polling()
 elif mode == "prod":
     def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))he heroku login
+        PORT = int(os.environ.get("PORT", "8443"))
         HEROKU_APP_NAME = os.environ.get("app-life-and-food")
         # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
         updater.start_webhook(listen="0.0.0.0",
